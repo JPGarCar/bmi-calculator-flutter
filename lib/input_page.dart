d4299a9969f5d72f9d8aca0e0784e083f0a359fd
Juan Pablo Garcia <juapgarc@gmail.com>
2019-12-18 20:12:08 -0800

    Learned to make my own widgets and have its own constructor to have different kinds of the same widget, also looked at how final makes a var not able to be changed once the widget or object for that matter is constructed!

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -12,8 +12,28 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(color: Color(0xFF1D1E33)),
+                ),
+                Expanded(child: ReusableCard(color: Color(0xFF1D1E33))),
+              ],
+            ),
+          ),
+          Expanded(child: ReusableCard(color: Color(0xFF1D1E33))),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(child: ReusableCard(color: Color(0xFF1D1E33))),
+                Expanded(child: ReusableCard(color: Color(0xFF1D1E33))),
+              ],
+            ),
+          ),
+        ],
       ),
       /*floatingActionButton: Theme(
         // to change something we can wrap with theme widget and pass a new theme
@@ -22,6 +42,28 @@ class _InputPageState extends State<InputPage> {
           child: Icon(Icons.add),
         ),
       ),*/
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  // no need to use key thus we use our won constructor
+  ReusableCard({@required this.color});
+
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        // Color has to go inside the box decoration
+        color: color,
+      ),
+      /* NOT USED TO USE THE EXPANDED WIDGET
+        height: 200.0,
+        width: 170.0,*/
     );
   }
 }
